cc861b233948fd553d7b319b4efe69ba817ce1e8
djfreeart81 <57782886+djfreeart81@users.noreply.github.com>
2021-04-15 12:57:44 -0400

    add deletion of cards
    if needed before creation

--- JS/game.js
+++ JS/game.js
@@ -4,8 +4,10 @@ import { Bank } from './Bank.js'
 
 const player1 = new Player('GG',1000)
 const player2 = new Player('Lenny',10)
-const cardDeck = new CardDeck(4)
+player2.id=2
+let cardDeck
 const bank = new Bank();
+
 console.log(`Player ${player1.name} created with id ${player1.id} and ${player1.money} $`)
 console.log(`Player ${player2.name} created with id ${player2.id} and ${player2.money} $`)
 console.log(`Deck created with ${cardDeck.length} cards`)
@@ -15,7 +17,7 @@ console.log(cardDeck)
 main();
 
 function main(){
-    initialize()
+    initializeCards()
 
     update()
     
@@ -23,10 +25,12 @@ function main(){
     console.log(`${cardDeck.length} remaining cards in Deck`)
 }
 
-function initialize(){
+function initializeCards(){
+    
+    
+    cardDeck = new CardDeck(4)
     player1.cards.push(cardDeck.getCard(), cardDeck.getCard())
     drawPlayer(player1)
-    player2.id=2
     player2.cards.push(cardDeck.getCard(), cardDeck.getCard())
     drawPlayer(player2)
     bank.cards.push(cardDeck.getCard(), cardDeck.getCard())
@@ -40,18 +44,41 @@ function update(){
 function drawPlayer(player){
     let $player = document.getElementById('player'+player.id)
     let $playerName = document.createElement('div')
-    let $playerMoney = document.createElement('div')
-    let $playerCards = document.createElement('div')
+    $playerName.id = `player${player.id}-name`
     $playerName.innerHTML = player.name
-    $playerMoney.innerHTML = player.money + " $"
-    $playerCards.innerHTML = player.cards.reduce((acc, i) => acc + i.image, "")
     $player.appendChild($playerName)
+    
+    let $playerMoney = document.createElement('div')
+    $playerMoney.id = `player${player.id}-money`
+    $playerMoney.innerHTML = player.money + " $"
     $player.appendChild($playerMoney)
+    
+    if (document.getElementById(`player${player.id}-cards`)) {
+        let cards = document.getElementById(`player${player.id}-cards`)
+        cards.remove()
+    }
+    let $playerCards = document.createElement('div')
+    $playerCards.id = = `player${player.id}-cards`
+    $playerCards.innerHTML = player.cards.reduce((acc, i) => acc + i.image, "")
     $player.appendChild($playerCards)
 }
 function drawBank(player){
     let $bank = document.getElementById('bank')
     let $bankCards = document.createElement('div')
+    $bankCards.id = `bank-cards`
     $bankCards.innerHTML = bank.cards.reduce((acc, i) => acc + i.image, "")
     $bank.appendChild($bankCards)
+}
+
+funtion clearCards(){
+    player1.cards = []
+    player2.cards = []
+    bank.cards = []
+    
+    for (let i=1; i<3; i++) {
+        let cards = document.getElementById(`player${i}-cards`)
+        cards.remove()
+    }
+    let $bank = document.getElementById('bank')
+    
 }
