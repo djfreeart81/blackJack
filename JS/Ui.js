7231f9db9dd7071712ca8f512a1870aa74abee49
djfreeart81 <57782886+djfreeart81@users.noreply.github.com>
2021-05-06 13:00:12 -0400

    Update Ui.js

--- JS/Ui.js
+++ JS/Ui.js
@@ -2,6 +2,17 @@ export class Ui {
     disableButtonById(id,bool){
         document.getElementById(id).disabled = bool
     }
+    
+    /*
+    ** playerId: id of the player
+    ** buttonId: array of the button ids to update
+    */
+    updateButtonById(player, buttonIds){
+        for (let buttonId in buttonIds){
+            document.getElementById(buttonId).disabled = player.buttonId
+            //TODO: get a link to the player status to update the button based on the player's status.
+        }
+    }
     newInfoMessage(message){
         document.getElementById("info").innerHTML = message
    }
