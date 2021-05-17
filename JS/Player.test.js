b4bd7aaee78cae8f8f3bb50d7dbe637fb78362a2
djfreeart81 <57782886+djfreeart81@users.noreply.github.com>
2021-05-17 12:34:57 -0400

    added 1 test

--- JS/Player.test.js
+++ JS/Player.test.js
@@ -18,3 +18,9 @@ test('cards 1 + 1 + 1 + 1 = 14', () => {
     player1.cards = [{value: 1, image: "image"}, {value: 1, image: "image"},{value: 1, image: "image"},{value: 1, image: "image"}]
     expect(player1.calculateScore()).toBe(14)
 })
+
+test('cards 1 + 5 + 5 = 21', () => {
+    const player1 = new Player("test", 1000)
+    player1.cards = [{value: 1, image: "image"}, {value: 5, image: "image"},{value: 5, image: "image"}]
+    expect(player1.calculateScore()).toBe(21)
+})
